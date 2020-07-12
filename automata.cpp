d5d943d373ddfb1a57a2bbbe8b128228a92c3592
KlejnotMopsu <67546276+KlejnotMopsu@users.noreply.github.com>
2020-07-12 03:38:02 +0200

    Add files via upload

---
+++ automata.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+class tFunction{
+private:
+public:
+	
+};
+
+
+class state{
+private:
+public:
+	char symbol;
+	
+};
+
+
+char alphabet[] = {'a', 'b'};
+
+
+
+
+//------------------------------------------------------
+int main(){
+while(true){
+char k;
+
+k = getch();
+
+if (k == '0') return 0;
+cout << "Wcisniety znak - " << k << endl;
+}
+}
+
